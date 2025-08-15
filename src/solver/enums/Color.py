0d344522f5bbaa77d02859298f70c2627ee87189
Ognian Baruh <ognian@baruh.net>
2025-08-16 00:34:45 +0300

    [FEATURE] Added main enumerations for handling a Rubik's cube

---
+++ src/solver/enums/Color.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Color(Enum):
+    WHITE = 'W'
+    YELLOW = 'Y'
+    ORANGE = 'O'
+    RED = 'R'
+    GREEN = 'G'
+    BLUE = 'B'
---
+++ src/solver/enums/Layer.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Layer(Enum):
+    UP = 'U'
+    DOWN = 'D'
+    LEFT = 'L'
+    RIGHT = 'R'
+    FRONT = 'F'
+    BACK = 'B'
