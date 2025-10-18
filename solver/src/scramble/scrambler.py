2178fb78b94b9fa5987603432f272cc52f2a516d
Baruh Ognian <bao1sf3@bosch.com>
2025-10-18 11:19:27 +0300

    [FEATURE] Use only one generic method for scrambling and several helper methods

--- solver/src/scramble/scrambler.py
+++ solver/src/scramble/scrambler.py
@@ -26,25 +26,95 @@ class Scrambler:
             Layer.RIGHT: Layer.LEFT,
         }
 
-    def generate_scramble(self, cube_size: int, scramble_length: int | None = None) -> list[Move]:
+    def generate_scramble(self, cube_size: int) -> list[Move]:
         """
         Generates a scramble for a cube of given size.
 
         :param cube_size: Size of the cube (e.g., 2 for 2x2, 3 for 3x3, etc.)
-        :param scramble_length: Length of the scramble (optional)
         :return: List of moves in the scramble
         """
 
-        match cube_size:
-            case 2:
-                return self.__generate_scramble_2x2(scramble_length or 8)
-            case 3:
-                return self.__generate_scramble_3x3(scramble_length or 20)
-            case _:
-                return self.__generate_scramble_nxn(cube_size, scramble_length or (cube_size - 2) * 20)
+        if cube_size < 2:
+            raise ValueError("Cube size must be at least 2.")
+
+        # Determine scramble length
+        scramble_length = self.__get_scramble_length(cube_size)
+
+        scramble: list[Move] = []
+        previous_faces: list[Move] = []
+        # Generate scramble
+        while len(scramble) < scramble_length:
+            # Create a random move
+            move = self.__generate_random_move(cube_size)
+
+            # If the move or its opposite is already in the previous faces, skip
+            if not self.__is_valid_random_move(move, previous_faces):
+                continue
+
+            # If a different move on the same axis is picked, append
+            if self.__should_append_to_previous_moves(move, previous_faces):
+                previous_faces.append(move)
+            # If any other face is picked, reset the previous_face list
+            else:
+                previous_faces = [move]
+
+            # Append the move to the scramble
+            scramble.append(move)
+
+        return scramble
 
     @staticmethod
-    def is_valid_random_move(move: Move, previous_moves: list[Move]) -> bool:
+    def __get_scramble_length(cube_size: int) -> int:
+        """
+        Determines the default scramble length based on cube size.
+
+        :param cube_size: Size of the cube
+        :return: Default scramble length
+        """
+
+        if cube_size < 2:
+            raise ValueError("Cube size must be at least 2.")
+
+        match cube_size:
+            case 2:
+                return 8
+            case 3:
+                return 20
+            case _:
+                return (cube_size - 2) * 20
+
+    def __generate_random_move(self, cube_size: int) -> Move:
+        """
+        Generates a random move for a cube of given size.
+
+        :param cube_size: Size of the cube
+        :return: A randomly generated move
+        """
+
+        if cube_size < 2:
+            raise ValueError("Cube size must be at least 2.")
+
+        # If the cube is 2x2, use only UP, FRONT, RIGHT faces
+        if cube_size == 2:
+            face = random.choice([Layer.UP, Layer.FRONT, Layer.RIGHT])
+        # Choose a random face
+        else:
+            face = random.choice([f for f in self.faces])
+
+        # Choose a random direction
+        direction = random.choice(self.directions)
+
+        # If the cube is 2x2 or 3x3, layer amount is always 1
+        if cube_size == 2 or cube_size == 3:
+            layer_amount = 1
+        # Choose a random layer amount
+        else:
+            layer_amount = random.randint(1, cube_size // 2)
+
+        return Move(layer=face, direction=direction, layer_amount=layer_amount)
+
+    @staticmethod
+    def __is_valid_random_move(move: Move, previous_moves: list[Move]) -> bool:
         """
         Checks if a randomly generated move is valid based on the previous move.
 
@@ -61,7 +131,7 @@ class Scrambler:
                 return False
         return True
 
-    def should_append_to_previous_moves(self, move: Move, previous_moves: list[Move]) -> bool:
+    def __should_append_to_previous_moves(self, move: Move, previous_moves: list[Move]) -> bool:
         """
         Determines if the current move should be appended to the list of previous moves.
 
@@ -76,120 +146,3 @@ class Scrambler:
         if move.layer == previous_moves[0].layer or move.layer == self.opposite_faces.get(previous_moves[0].layer):
             return True
         return False
-
-    def __generate_scramble_2x2(self, scramble_length: int) -> list[Move]:
-        """
-        Generates a scramble for a 2x2 cube.
-
-        :param scramble_length: Length of the scramble
-        :return: List of moves in the scramble
-        """
-
-        faces = [Layer.UP, Layer.FRONT, Layer.RIGHT]
-
-        scramble: list[Move] = []
-        previous_faces: list[Move] = []
-
-        while len(scramble) < scramble_length:
-            # Choose a random face
-            face = random.choice([f for f in faces])
-
-            # Choose a random direction
-            direction = random.choice(self.directions)
-
-            # Create the move
-            move = Move(layer=face, direction=direction, layer_amount=1)
-
-            # If the move or its opposite is already in the previous faces, skip
-            if not self.is_valid_random_move(move, previous_faces):
-                continue
-
-            # If a different move on the same axis is picked, append
-            if self.should_append_to_previous_moves(move, previous_faces):
-                previous_faces.append(move)
-            # If any other face is picked, reset the previous_face list
-            else:
-                previous_faces = [move]
-
-            # Append the move to the scramble
-            scramble.append(move)
-
-        return scramble
-
-    def __generate_scramble_3x3(self, scramble_length: int) -> list[Move]:
-        """
-        Generates a scramble for a 3x3 cube.
-
-        :param scramble_length: Length of the scramble
-        :return: List of moves in the scramble
-        """
-
-        scramble: list[Move] = []
-        previous_faces: list[Move] = []
-
-        while len(scramble) < scramble_length:
-            # Choose a random face
-            face = random.choice([f for f in self.faces])
-
-            # Choose a random direction
-            direction = random.choice(self.directions)
-
-            # Create the move
-            move = Move(layer=face, direction=direction, layer_amount=1)
-
-            # If the move or its opposite is already in the previous faces, skip
-            if not self.is_valid_random_move(move, previous_faces):
-                continue
-
-            # If a different move on the same axis is picked, append
-            if self.should_append_to_previous_moves(move, previous_faces):
-                previous_faces.append(move)
-            # If any other face is picked, reset the previous_face list
-            else:
-                previous_faces = [move]
-
-            # Append the move to the scramble
-            scramble.append(move)
-
-        return scramble
-
-    def __generate_scramble_nxn(self, cube_size: int, scramble_length: int) -> list[Move]:
-        """
-        Generates a scramble for an NxN cube.
-
-        :param cube_size: Size of the cube
-        :param scramble_length: Length of the scramble
-        :return: List of moves in the scramble
-        """
-
-        scramble: list[Move] = []
-        previous_faces: list[Move] = []
-
-        while len(scramble) < scramble_length:
-            # Choose a random face
-            face = random.choice([f for f in self.faces])
-
-            # Choose a random direction
-            direction = random.choice(self.directions)
-
-            # Choose a random layer amount
-            layer_amount = random.randint(1, cube_size // 2)
-
-            # Create the move
-            move = Move(layer=face, direction=direction, layer_amount=layer_amount)
-
-            # If the move or its opposite is already in the previous faces, skip
-            if not self.is_valid_random_move(move, previous_faces):
-                continue
-
-            # If a different move on the same axis is picked, append
-            if self.should_append_to_previous_moves(move, previous_faces):
-                previous_faces.append(move)
-            # If any other face is picked, reset the previous_face list
-            else:
-                previous_faces = [move]
-
-            # Append the move to the scramble
-            scramble.append(move)
-
-        return scramble
