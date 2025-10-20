fc44dc2e6028102c96d95c05371867fb8025b5d6
Baruh Ognian <bao1sf3@bosch.com>
2025-10-20 16:43:39 +0300

    [FEATURE] Create a Cube class with constructor and show method

--- visualizer/src/public/js/sketch.js
+++ visualizer/src/public/js/sketch.js
@@ -5,25 +5,14 @@ let cube = [];
 window.setup = () => {
     window.setupCanvas();
 
-    for (let x = -1, xIndex = 0; x <= 1; x++, xIndex++) {
-        cube[xIndex] = [];
-        for (let y = -1, yIndex = 0; y <= 1; y++, yIndex++) {
-            cube[xIndex][yIndex] = [];
-            for (let z = -1, zIndex = 0; z <= 1; z++, zIndex++) {
-                cube[xIndex][yIndex][zIndex] = new window.Piece(x, y, z);
-            }
-        }
-    }
+    cube = new Cube(dim);
 
-    cube[0][0][2].highlighted = true;
+    cube.pieces[2].highlighted = true;
 }
 
 window.draw = () => {
     window.setBackground();
     window.scale(50);
 
-    for (let i = 0; i < dim; i++)
-        for (let j = 0; j < dim; j++)
-            for (let k = 0; k < dim; k++)
-                cube[i][j][k].show();
+    cube.show()
 }
