97667daa6be42312ab3ea17feb20552be2b01294
Baruh Ognian <bao1sf3@bosch.com>
2025-10-20 18:11:09 +0300

    [FEATURE] Add enumerations for layer turned and direction of the turn

---
+++ visualizer/src/public/js/enums/direction.js
@@ -0,0 +1,5 @@
+const Direction = Object.freeze({
+    CW: '',
+    CCW: '\'',
+    DOUBLE: '2'
+})
---
+++ visualizer/src/public/js/enums/layer.js
@@ -0,0 +1,8 @@
+const Layer = Object.freeze({
+    U: 'U',
+    D: 'D',
+    F: 'F',
+    B: 'B',
+    L: 'L',
+    R: 'R',
+})
