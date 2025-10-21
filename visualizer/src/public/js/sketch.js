19cd03e5cd7ea6c7e99540060a31c8a6a03b8e76
Baruh Ognian <bao1sf3@bosch.com>
2025-10-21 23:13:29 +0300

    [FEATURE] Implement a Move class with a parser method and a getter for the rotation of the cube

--- visualizer/src/public/js/sketch.js
+++ visualizer/src/public/js/sketch.js
@@ -12,6 +12,21 @@ window.setup = () => {
         cube.pieces[i + 1].highlighted = "green";
         cube.pieces[i + 2].highlighted = "blue";
     }
+
+    // let scramble = "R D2 L2 U F' L D U2 B R2 U2 D2 R2 B R2 B2 U L2 B Rw2 Uw2 D Rw2 R' B2 Rw2 D' R2 D2 R2 U2 Fw D2 L' Fw Uw2 Rw Uw2 F2 L D Rw' Uw";
+    let scramble = "R U Dw' Uw2 3Rw 5Lw'";
+    let splitScramble = scramble.split(" ");
+    let moves = [];
+    splitScramble.forEach(moveText => moves.push(new Move(moveText)));
+
+    moves.forEach(move => {
+        console.log(move.layer);
+        console.log(move.direction);
+        console.log(move.layerAmount);
+        console.log(typeof move.layer);
+        console.log(typeof move.direction);
+        console.log(typeof move.layerAmount);
+    })
 }
 
 window.draw = () => {
