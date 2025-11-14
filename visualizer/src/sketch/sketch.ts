2fd5c78cb8052989dc26e3bfe67a3c26d84e543c
Baruh Ognian <bao1sf3@bosch.com>
2025-11-14 17:27:39 +0200

    [FEATURE] Remove key press handler from the sketch

--- visualizer/src/sketch/sketch.ts
+++ visualizer/src/sketch/sketch.ts
@@ -131,26 +131,4 @@ export const cubeSketch = (p: p5) => {
             console.log("Connection closed:", event.reason);
         }
     }
-
-    /**
-     * Key pressed event handler for the p5 sketch
-     * @param key - The keyboard event
-     *
-     * @example
-     * p.keyPressed();
-     */
-    p.keyPressed = (key: KeyboardEvent) : void => {
-        console.log(`Key pressed: ${key.key}`);
-        if (key.key === 's') {
-            let scramble = "D2 B' U' L' F2 B L U2 L' R2 F' D2 F' L2 B L2 D2 B D2 F R2";
-            // let scramble1 = "Dw2 B' U' L' F2 B Lw U2 L' R2 F' D2 Fw' L2 Bw L2 D2 B D2 F R2";
-            // let scramble2 = "3Rw' Dw D Bw B Fw 3Bw 3Dw' Bw2 Rw' R2 3Fw' B' Fw Uw' Rw' 3Dw D Lw2 Dw' Lw' Rw' L D' F' Uw' 3Uw"
-            // let scramble3 = "R' D2 L Rw' Dw2 Bw' 3Lw2 F2 B2 Bw' 3Uw Dw' Fw' Uw' Rw' 3Dw 3Fw D 3Fw 3Dw2 Bw R' Rw' Bw2";
-            // let scramble4 = "F2 3Fw D Uw Lw' Uw2 D2 3Bw Bw' D' U2 Uw' Bw D 3Bw Uw' 3Lw2 D2 3Dw2 F Lw2 3Dw' 3Uw2 F' R2;"
-            // let scramble5 = "3Uw2 3Bw 3Uw2 L2 R' U Uw 3Dw' F2 3Uw' B R2 D2 Uw 3Dw2 L' 3Fw2 3Rw2 Bw Dw Rw2 B' L' R'"
-            // let scramble = "3Lw' Dw2 Rw2 3Rw2 3Uw2 L' Fw' Uw2 3Rw2 3Lw2 R Dw Fw F2 3Dw' Lw2 Bw2 3Rw2 Dw' 3Rw F' 3Bw2 Bw' U Bw Uw' 3Rw2 Dw' 3Dw2 U' 3Rw 3Fw2 Bw Lw 3Fw 3Lw' Fw Rw Lw2 R U Lw U D2 3Rw2 Uw U' B' L2 3Fw 3Bw D Bw2 D' L R2 3Rw' B Bw 3Uw Dw2 B' L 3Lw' D' Lw' 3Fw' 3Uw Fw2 Bw2 U2 Rw Fw B' 3Rw2 3Bw2 L2 3Bw 3Uw2 B2 L 3Uw2 U2 Bw2 Rw2 3Rw' 3Lw2 3Uw 3Rw Bw D' B' L' 3Fw R U 3Dw 3Bw2 U' Dw'"
-            // let scramble7 = "U D'";
-            cube.addMovesFromString(scramble);
-        }
-    };
 }
